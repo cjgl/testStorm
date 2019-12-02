09ba969190911dd9a52f1e003500c0f266185b42
cjgl <114471855@qq.com>
2019-12-02 15:35:00 +0800

    update

--- src/main/java/cn/fy/WordCountTopology.java
+++ src/main/java/cn/fy/WordCountTopology.java
@@ -1,5 +1,7 @@
 package cn.fy;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
@@ -21,112 +23,125 @@ import java.util.List;
 import java.util.Map;
 
 public class WordCountTopology {
-  public static class SplitSentence implements IRichBolt {
-    private OutputCollector _collector;
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-      declarer.declare(new Fields("word"));
-    }
+	public static final Log log = LogFactory.getLog(WordCountTopology.class);
 
-    public Map<String, Object> getComponentConfiguration() {
-      return null;
-    }
+	public static class SplitSentence implements IRichBolt {
+		private OutputCollector _collector;
 
-    public void prepare(Map stormConf, TopologyContext context,
-            OutputCollector collector) {
-        _collector = collector;
-    }
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			declarer.declare(new Fields("word"));
+		}
 
-    public void execute(Tuple input) {
-        String sentence = input.getStringByField("word");
-        String[] words = sentence.split(" ");
-        for(String word : words){
-            this._collector.emit(new Values(word));
-        }
-    }
+		public Map<String, Object> getComponentConfiguration() {
+			return null;
+		}
 
-    public void cleanup() {
-        // TODO Auto-generated method stub
-        
-    }
-  }
+		public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+			_collector = collector;
+		}
 
-  public static class WordCount extends BaseBasicBolt {
-    Map<String, Integer> counts = new HashMap<String, Integer>();
+		public void execute(Tuple input) {
+			String sentence = input.getStringByField("word");
+			String[] words = sentence.split(" ");
+			for (String word : words) {
+				this._collector.emit(new Values(word));
+			}
+		}
 
-    public void execute(Tuple tuple, BasicOutputCollector collector) {
-      String word = tuple.getString(0);
-      Integer count = counts.get(word);
-      if (count == null)
-        count = 0;
-      count++;
-      counts.put(word, count);
-      collector.emit(new Values(word, count));
-    }
+		public void cleanup() {
+			// TODO Auto-generated method stub
 
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-      declarer.declare(new Fields("word", "count"));
-    }
-  }
-  
-  public static class WordReport extends BaseBasicBolt {
-        Map<String, Integer> counts = new HashMap<String, Integer>();
+		}
+	}
 
-        public void execute(Tuple tuple, BasicOutputCollector collector) {
-          String word = tuple.getStringByField("word");
-          Integer count = tuple.getIntegerByField("count");
-          this.counts.put(word, count);
-        }
+	public static class WordCount extends BaseBasicBolt {
+		public static final Log _log = LogFactory.getLog(WordCount.class);
+		
+		Map<String, Integer> counts = new HashMap<String, Integer>();
 
-        public void declareOutputFields(OutputFieldsDeclarer declarer) {
-            
-        }
-        
-        @Override
-        public void cleanup() {
-            System.out.println("-----------------FINAL COUNTS  START-----------------------");
-            List<String> keys = new ArrayList<String>();
-            keys.addAll(this.counts.keySet());
-            Collections.sort(keys);
-            
-            for(String key : keys){
-                System.out.println(key + " : " + this.counts.get(key));
-            }
-            
-            System.out.println("-----------------FINAL COUNTS  END-----------------------");
-        } 
+		public void execute(Tuple tuple, BasicOutputCollector collector) {
+			String word = tuple.getString(0);
+			Integer count = counts.get(word);
+			if (count == null)
+				count = 0;
+			count++;
+			counts.put(word, count);
+			collector.emit(new Values(word, count));
+		}
 
-      }
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			declarer.declare(new Fields("word", "count"));
+		}
 
-  public static void main(String[] args) throws Exception {
+		@Override
+		public void cleanup() {
+			// TODO Auto-generated method stub
+			_log.info(">>>>>>>>>>WordCount cleanup<<<<<<<<<<");
+			super.cleanup();
+		}
 
-    TopologyBuilder builder = new TopologyBuilder();
+	}
 
-    builder.setSpout("spout", new RandomSentenceSpout(), 5);
+	public static class WordReport extends BaseBasicBolt {
+		public static final Log _log = LogFactory.getLog(WordReport.class);
+		
+		Map<String, Integer> counts = new HashMap<String, Integer>();
 
-    //ShuffleGrouping：随机选择一个Task来发送。
-    builder.setBolt("split", new SplitSentence(), 8).shuffleGrouping("spout");
-    //FiledGrouping：根据Tuple中Fields来做一致性hash，相同hash值的Tuple被发送到相同的Task。
-    builder.setBolt("count", new WordCount(), 12).fieldsGrouping("split", new Fields("word"));
-    //GlobalGrouping：所有的Tuple会被发送到某个Bolt中的id最小的那个Task。
-    builder.setBolt("report", new WordReport(), 6).globalGrouping("count");
+		public void execute(Tuple tuple, BasicOutputCollector collector) {
+			String word = tuple.getStringByField("word");
+			Integer count = tuple.getIntegerByField("count");
+			this.counts.put(word, count);
+		}
 
-    Config conf = new Config();
-    conf.setDebug(true);
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
 
-    if (args != null && args.length > 0) {
-      conf.setNumWorkers(3);
+		}
 
-      StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
-    }
-    else {
-      conf.setMaxTaskParallelism(3);
+		@Override
+		public void cleanup() {
+			_log.info("-----------------FINAL COUNTS  START-----------------------");
+			List<String> keys = new ArrayList<String>();
+			keys.addAll(this.counts.keySet());
+			Collections.sort(keys);
 
-      LocalCluster cluster = new LocalCluster();
-      cluster.submitTopology("word-count", conf, builder.createTopology());
+			for (String key : keys) {
+				System.out.println(key + " : " + this.counts.get(key));
+			}
 
-      Thread.sleep(20000);
+			_log.info("-----------------FINAL COUNTS  END-----------------------");
+		}
 
-      cluster.shutdown();
-    }
-  }
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		TopologyBuilder builder = new TopologyBuilder();
+
+		builder.setSpout("spout", new RandomSentenceSpout(), 2);
+
+		// ShuffleGrouping：随机选择一个Task来发送。
+		builder.setBolt("split", new SplitSentence(), 4).shuffleGrouping("spout");
+		// FiledGrouping：根据Tuple中Fields来做一致性hash，相同hash值的Tuple被发送到相同的Task。
+		builder.setBolt("count", new WordCount(), 5).fieldsGrouping("split", new Fields("word"));
+		// GlobalGrouping：所有的Tuple会被发送到某个Bolt中的id最小的那个Task。
+		builder.setBolt("report", new WordReport(), 4).globalGrouping("count");
+
+		Config conf = new Config();
+		conf.setDebug(true);
+
+		if (args != null && args.length > 0) {
+			conf.setNumWorkers(3);
+
+			StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+		} else {
+			conf.setMaxTaskParallelism(3);
+
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology("word-count", conf, builder.createTopology());
+
+			Thread.sleep(20000);
+
+			cluster.shutdown();
+		}
+	}
 }
